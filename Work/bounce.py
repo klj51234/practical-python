fa0b99f0e1cc1ccfde6f47e6409a489927ee3119
klj51234 <709878299@qq.com>
2023-03-16 15:41:59 +0800

    1.0

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+
+# time = 0
+# while time < 10:
+#     height = height * 3 / 5
+#     time = time + 1
+#     print(round(height, 4))
+
+for i in range(0, 10, 1):
+    height = height * 3 / 5
+    print(round(height, 4))
---
+++ Work/test.py
@@ -0,0 +1,7 @@
+# rate = 12
+# a = (1 + rate / 12)
+# print(a)
+
+# result is True
+print(bool("False"))
+
